4da79b4d539be7005a8b7e0ebd9320de6b857ac2
Janhavi Talhar <60478538+T-janhavi11@users.noreply.github.com>
2020-11-02 16:56:07 +0530

    Add files via upload

---
+++ pages/index.php
@@ -0,0 +1,125 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@500&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Lobster&display=swap" rel="stylesheet">
+    <title>AF</title>
+  </head>
+  <body>
+  <!-- Navigation bar --> 
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="#">ArmyFirst</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="login.php">Login</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="about.php">About</a>
+      </li>
+     
+ 
+    </ul>
+    
+  </div>
+</nav>
+
+
+<!--Caroussel-->
+
+<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel" style="padding: 6px;">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+        
+      <img class="d-block w-100" src="mitul-gajera-R8Qs3QRuKoM-unsplash.jpg" alt="First slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="curioso-photography-xYrTdhysQY0-unsplash.jpg" alt="Second slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="daniel-stub--9ZbwBDOX9c-unsplash.jpg" alt="Third slide">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+
+<!--Details-->
+<section class="my-5">
+    <div class="py-5">
+        <h1 class="text-center">Want to know about the website?</h1>
+    </div>
+
+    <div class="container-fluid">
+    <div class="row">
+      
+        <div class="col-lg-6 col-md-6 col-12">
+           <p class="py-4">  <h4 class="display-4"> General Sharad Narang </h4> 
+           ArmyFirst was founded in the memory of late General Sharad Narang. asdfghjklpoiuy dfghjkohg vef uy f c scandir
+           yf byes vhb vu v dh bvvgdvdvhbdhv jjh vh vhv hbvuh jv ghdvhkd v svh gsuvb hs vjhsb sbjsjhz bjhz vdsdc
+           sdfghhb  v bdsrtfghv xfg hv rtcfghr fcgvbrt cfgjvr cfjgh xhfcgm rcjghvf ykvh g vjh,gl j,hv jhv, rjcgmbt fghcm
+           r fgcj tcjghmbt cghvm tjghvmbnt ghv ythgvnb cghbj cghmj cghmb cghmb gchmb cgmb gcmb cgmb gchmb
+           ugjhvb cbhjxdvjdvhuidvjbdhbjh vhjb djv hj vgh vzdh zs vchzvh zvhz bdvhzjhv zdhv  hzvchgz vchgzv gzgvhsbvch
+           asdfghjklpoiuy dfghjkohg vef uy f c scandir
+           yf byes vhb vu v dh bvvgdvdvhbdhv jjh vh vhv hbvuh jv ghdvhkd v svh gsuvb hs vjhsb sbjsjhz bjhz vdsdc
+           sdfghhb  v bdsrtfghv xfg hv rtcfghr fcgvbrt cfgjvr cfjgh xhfcgm rcjghvf ykvh g vjh,gl j,hv jhv, rjcgmbt fghcm
+           r fgcj tcjghmbt cghvm tjghvmbnt ghv ythgvnb cghbj cghmj cghmb cghmb gchmb cgmb gcmb cgmb gchmb
+           ugjhvb cbhjxdvjdvhuidvjbdhbjh vhjb djv hj vgh vzdh zs vchzvh zvhz bdvhzjhv zdhv  hzvchgz vchgzv gzgvhsbvch
+           asdfghjklpoiuy dfghjkohg vef uy f c scandir
+           yf byes vhb vu v dh bvvgdvdvhbdhv jjh vh vhv hbvuh jv ghdvhkd v svh gsuvb hs vjhsb sbjsjhz bjhz vdsdc
+           sdfghhb  v bdsrtfghv xfg hv rtcfghr fcgvbrt cfgjvr cfjgh xhfcgm rcjghvf ykvh g vjh,gl j,hv jhv, rjcgmbt fghcm
+           r fgcj tcjghmbt cghvm tjghvmbnt ghv ythgvnb cghbj cghmj cghmb cghmb gchmb cgmb gcmb cgmb gchmb
+           ugjhvb cbhjxdvjdvhuidvjbdhbjh vhjb djv hj vgh vzdh zs vchzvh zvhz bdvhzjhv zdhv  hzvchgz vchgzv gzgvhsbvch
+           </p>
+           <p class="py-5">
+            <p><button type="button" class="btn btn-secondary" ><a href="about.php">Read More</a></button></p>
+           </p>
+        </div>
+    </div>
+    <style>
+.footer {
+  position: fixed;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  background-color: black;
+  color: white;
+  text-align: center;
+}
+</style>
+<footer class="footer">
+  <a href="login.php">Contact us</a>
+</footer>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  </body>
+</html>
---
+++ pages/insidepage.php
@@ -0,0 +1,120 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@500&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Lobster&display=swap" rel="stylesheet">
+    <title>AF</title>
+  </head>
+  <body>
+
+  <!-- Navigation bar --> 
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="#">ArmyFirst</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav ml-auto">
+     
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Check
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+          <a class="dropdown-item" href="#">Weapon details</a>
+          <a class="dropdown-item" href="#">Company details</a>
+          <a class="dropdown-item" href="#">Batallion details</a>
+        </div>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="index.php">Sign out <span class="sr-only">(current)</span></a>
+      </li>
+     
+ 
+    </ul>
+    
+  </div>
+</nav>
+
+<h1></h1>
+
+
+
+   </div>
+
+   <div class="card-group">
+  <div class="card" >
+    <img src="milad-shams-tF1QiNdjT7A-unsplash.jpg" class="card-img-top" style=" width: 40% ;"  alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Genral Sharad Narang</h5>
+      <p class="card-text">Welcome! you have successfully logged in to your acccount.</p>
+      <p class="card-text"><small class="text-muted">Last logined 30 minutes ago</small></p>
+    </div>
+  </div>
+  <div class="card">
+    
+    <div class="card-body">
+      <h5 class="card-title">SHARAD NARANG -SD1001</h5>
+      <p class="card-text">
+          <h3> Your Info</h3>
+        <ul>
+        <li> Name: Sharad Narang</li> 
+        <li>  Batch-name: 445</li>
+        <li>  Batch-year: 2018</li>
+        <li>  Work-type: Captain</li>
+             ..Captain ID: 660
+         <li> War-status: Inactive</li>
+         <li> Last posting: Kargil</li>
+          <li>Birthday: 9-April-1997</li>
+          <li>Birth-place: Agra</li>
+          <li>Gender: Male</li>
+          <li>Medals under records: Indian Order of Merit, Kirti chakra</li>
+        </ul>
+        <button type="button" class="btn btn-secondary">Update Info</button>
+        </p>
+      <p class="card-text"><small class="text-muted">Last updated 30 mins ago</small></p>
+    </div>
+  </div>
+   </div>
+<hr>
+<h3>Search for some other soldier</h3>
+<p>Learn about the basic info and war status of your fellow soldiers</p>
+<div style= "padding-bottom: 100px;">
+<form class="form-inline my-2 my-lg-0" >
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+</div>
+
+<!--footer-->
+<style>
+.footer {
+  position: fixed;
+  padding-top: 15px;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  background-color: black;
+  color: white;
+  text-align: center;
+}
+</style>
+<footer class="footer">
+  <a href="application.php">Contact us</a>
+</footer>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  </body>
+</html>
